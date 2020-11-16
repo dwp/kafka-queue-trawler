39e7a02290a02d57a507b546153a4d8480e19e85
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2020-11-16 09:47:45 +0000

    Initial commit, adding githooks submodule

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# dataworks-repo-template-docker
+# kafka-queue-trawler
 
-## Description
+## Simple kafka queue trawler that lists records on the queue.
 
 This repo contains Makefile, and Dockerfile to fit the standard pattern.
 This repo is a base to create new Docker image repos, adding the githooks submodule, making the repo ready for use.
--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "dataworks-repo-template-docker" {
-  name = "dataworks-repo-template-docker"
+resource "aws_ecr_repository" "kafka-queue-trawler" {
+  name = "kafka-queue-trawler"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/dataworks-repo-template-docker" }
+    { DockerHub : "dwpdigital/kafka-queue-trawler" }
   )
 }
 
-resource "aws_ecr_repository_policy" "dataworks-repo-template-docker" {
-  repository = aws_ecr_repository.dataworks-repo-template-docker.name
+resource "aws_ecr_repository_policy" "kafka-queue-trawler" {
+  repository = aws_ecr_repository.kafka-queue-trawler.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
 output "ecr_example_url" {
-  value = aws_ecr_repository.dataworks-repo-template-docker.repository_url
+  value = aws_ecr_repository.kafka-queue-trawler.repository_url
 }
